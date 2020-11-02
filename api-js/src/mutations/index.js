5fdb5301cd9f19bcff1e7aa22c72b1bd601fa274
nsdeschenes <nsdeschenes@outlook.com>
2020-11-02 15:20:18 -0400

    mutation factory function completed

--- api-js/src/mutations/index.js
+++ api-js/src/mutations/index.js
@@ -1,24 +1,57 @@
 const { GraphQLObjectType } = require('graphql')
-const domainMutations = require('./domain')
-const organizationMutations = require('./organizations')
-const scanMutations = require('./scans')
-const userMutations = require('./user')
-const userAffiliationMutations = require('./user-affiliations')
+const { t } = require('@lingui/macro')
+const { createDomain, removeDomain, updateDomain } = require('./domain')
+const {
+  createOrganization,
+  removeOrganization,
+  updateOrganization,
+} = require('./organizations')
+const { requestScan } = require('./scans')
+const {
+  authenticate,
+  resetPassword,
+  sendEmailVerification,
+  sendPasswordResetLink,
+  sendPhoneCode,
+  signIn,
+  signUp,
+  updateUserPassword,
+  updateUserProfile,
+  verifyAccount,
+  verifyPhoneNumber,
+} = require('./user')
+const { inviteUserToOrg, updateUserRole } = require('./user-affiliations')
 
 const createMutationSchema = (i18n) => {
   return new GraphQLObjectType({
     name: 'Mutation',
+    description: i18n._(t`Root Mutation Object`),
     fields: () => ({
       // Domain Mutations
-      ...domainMutations,
+      createDomain: createDomain(i18n),
+      removeDomain: removeDomain(i18n),
+      updateDomain: updateDomain(i18n),
       // Organization Mutations
-      ...organizationMutations,
+      createOrganization: createOrganization(i18n),
+      removeOrganization: removeOrganization(i18n),
+      updateOrganization: updateOrganization(i18n),
       // Scan Mutations
-      ...scanMutations,
+      requestScan: requestScan(i18n),
       // User Mutations
-      ...userMutations,
+      authenticate: authenticate(i18n),
+      resetPassword: resetPassword(i18n),
+      sendEmailVerification: sendEmailVerification(i18n),
+      sendPasswordResetLink: sendPasswordResetLink(i18n),
+      sendPhoneCode: sendPhoneCode(i18n),
+      signIn: signIn(i18n),
+      signUp: signUp(i18n),
+      updateUserPassword: updateUserPassword(i18n),
+      updateUserProfile: updateUserProfile(i18n),
+      verifyAccount: verifyAccount(i18n),
+      verifyPhoneNumber: verifyPhoneNumber(i18n),
       // User Affiliations Mutations
-      ...userAffiliationMutations,
+      inviteUserToOrg: inviteUserToOrg(i18n),
+      updateUserRole: updateUserRole(i18n),
     }),
   })
 }
