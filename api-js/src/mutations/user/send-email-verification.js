1de61df75b0a78db03e1e98c38ba770f10524240
nsdeschenes <nsdeschenes@outlook.com>
2020-11-02 15:14:52 -0400

    user mutation docs translated

--- api-js/src/mutations/user/send-email-verification.js
+++ api-js/src/mutations/user/send-email-verification.js
@@ -3,76 +3,83 @@ const { mutationWithClientMutationId } = require('graphql-relay')
 const { GraphQLEmailAddress } = require('graphql-scalars')
 const { t } = require('@lingui/macro')
 
-const sendEmailVerification = new mutationWithClientMutationId({
-  name: 'SendEmailVerification',
-  description:
-    'This mutation is used for re-sending a verification email if it failed during user creation.',
-  inputFields: () => ({
-    userName: {
-      type: GraphQLNonNull(GraphQLEmailAddress),
-      description:
-        'The users email address used for sending the verification email.',
-    },
-  }),
-  outputFields: () => ({
-    status: {
-      type: GraphQLString,
-      description: 'Informs the user if the email was sent successfully.',
-      resolve: async (payload) => {
-        return payload.status
+const sendEmailVerification = (i18n) =>
+  new mutationWithClientMutationId({
+    name: 'SendEmailVerification',
+    description: i18n._(
+      t`This mutation is used for re-sending a verification email if it failed during user creation.`,
+    ),
+    inputFields: () => ({
+      userName: {
+        type: GraphQLNonNull(GraphQLEmailAddress),
+        description: i18n._(
+          t`The users email address used for sending the verification email.`,
+        ),
       },
-    },
-  }),
-  mutateAndGetPayload: async (
-    args,
-    {
-      i18n,
-      request,
-      auth: { tokenize },
-      validators: { cleanseInput },
-      loaders: { userLoaderByUserName },
-      notify: { sendVerificationEmail },
-    },
-  ) => {
-    // Cleanse Input
-    const userName = cleanseInput(args.userName).toLowerCase()
+    }),
+    outputFields: () => ({
+      status: {
+        type: GraphQLString,
+        description: i18n._(
+          t`Informs the user if the email was sent successfully.`,
+        ),
+        resolve: async (payload) => {
+          return payload.status
+        },
+      },
+    }),
+    mutateAndGetPayload: async (
+      args,
+      {
+        i18n,
+        request,
+        auth: { tokenize },
+        validators: { cleanseInput },
+        loaders: { userLoaderByUserName },
+        notify: { sendVerificationEmail },
+      },
+    ) => {
+      // Cleanse Input
+      const userName = cleanseInput(args.userName).toLowerCase()
 
-    // Get user from db
-    const user = await userLoaderByUserName.load(userName)
+      // Get user from db
+      const user = await userLoaderByUserName.load(userName)
 
-    if (typeof user !== 'undefined') {
-      // Choose template based on users preferred language
-      let templateId
-      if (user.preferredLang === 'french') {
-        templateId = 'f2c9b64a-c754-4ffd-93e9-33fdb0b5ae0b'
+      if (typeof user !== 'undefined') {
+        // Choose template based on users preferred language
+        let templateId
+        if (user.preferredLang === 'french') {
+          templateId = 'f2c9b64a-c754-4ffd-93e9-33fdb0b5ae0b'
+        } else {
+          templateId = '6e3368a7-0d75-47b1-b4b2-878234e554c9'
+        }
+
+        const token = tokenize({
+          parameters: { userId: user._key },
+        })
+
+        const verifyUrl = `${request.protocol}://${request.get(
+          'host',
+        )}/validate/${token}`
+
+        await sendVerificationEmail({ templateId, user, verifyUrl })
+
+        console.info(
+          `User: ${user._key} successfully sent a verification email.`,
+        )
       } else {
-        templateId = '6e3368a7-0d75-47b1-b4b2-878234e554c9'
+        console.warn(
+          `A user attempted to send a verification email for ${userName} but no account is affiliated with this user name.`,
+        )
       }
 
-      const token = tokenize({
-        parameters: { userId: user._key },
-      })
-
-      const verifyUrl = `${request.protocol}://${request.get(
-        'host',
-      )}/validate/${token}`
-
-      await sendVerificationEmail({ templateId, user, verifyUrl })
-
-      console.info(`User: ${user._key} successfully sent a verification email.`)
-    } else {
-      console.warn(
-        `A user attempted to send a verification email for ${userName} but no account is affiliated with this user name.`,
-      )
-    }
-
-    return {
-      status: i18n._(
-        t`If an account with this username is found, an email verification link will be found in your inbox.`,
-      ),
-    }
-  },
-})
+      return {
+        status: i18n._(
+          t`If an account with this username is found, an email verification link will be found in your inbox.`,
+        ),
+      }
+    },
+  })
 
 module.exports = {
   sendEmailVerification,
--- api-js/src/mutations/user/update-user-profile.js
+++ api-js/src/mutations/user/update-user-profile.js
@@ -4,94 +4,103 @@ const { GraphQLEmailAddress } = require('graphql-scalars')
 const { t } = require('@lingui/macro')
 const { LanguageEnums } = require('../../enums')
 
-const updateUserProfile = new mutationWithClientMutationId({
-  name: 'UpdateUserProfile',
-  description:
-    'This mutation allows the user to update their user profile to change various details of their current profile.',
-  inputFields: () => ({
-    displayName: {
-      type: GraphQLString,
-      description: 'The updated display name the user wishes to change to.',
-    },
-    userName: {
-      type: GraphQLEmailAddress,
-      description: 'The updated user name the user wishes to change to.',
-    },
-    preferredLang: {
-      type: LanguageEnums,
-      description:
-        'The updated preferred language the user wishes to change to.',
-    },
-  }),
-  outputFields: () => ({
-    status: {
-      type: GraphQLString,
-      description: 'The status if the user profile update was successful.',
-      resolve: async (payload) => {
-        return payload.status
+const updateUserProfile = (i18n) =>
+  new mutationWithClientMutationId({
+    name: 'UpdateUserProfile',
+    description: i18n._(
+      t`This mutation allows the user to update their user profile to change various details of their current profile.`,
+    ),
+    inputFields: () => ({
+      displayName: {
+        type: GraphQLString,
+        description: i18n._(
+          t`The updated display name the user wishes to change to.`,
+        ),
       },
-    },
-  }),
-  mutateAndGetPayload: async (
-    args,
-    {
-      i18n,
-      query,
-      userId,
-      loaders: { userLoaderByKey },
-      validators: { cleanseInput },
-    },
-  ) => {
-    // Cleanse Input
-    const displayName = cleanseInput(args.displayName)
-    const userName = cleanseInput(args.userName).toLowerCase()
-    const preferredLang = cleanseInput(args.preferredLang)
+      userName: {
+        type: GraphQLEmailAddress,
+        description: i18n._(
+          t`The updated user name the user wishes to change to.`,
+        ),
+      },
+      preferredLang: {
+        type: LanguageEnums,
+        description: i18n._(
+          t`The updated preferred language the user wishes to change to.`,
+        ),
+      },
+    }),
+    outputFields: () => ({
+      status: {
+        type: GraphQLString,
+        description: i18n._(
+          t`The status if the user profile update was successful.`,
+        ),
+        resolve: async (payload) => {
+          return payload.status
+        },
+      },
+    }),
+    mutateAndGetPayload: async (
+      args,
+      {
+        i18n,
+        query,
+        userId,
+        loaders: { userLoaderByKey },
+        validators: { cleanseInput },
+      },
+    ) => {
+      // Cleanse Input
+      const displayName = cleanseInput(args.displayName)
+      const userName = cleanseInput(args.userName).toLowerCase()
+      const preferredLang = cleanseInput(args.preferredLang)
 
-    // Make sure userId is not undefined
-    if (typeof userId === 'undefined') {
-      console.warn(
-        `User attempted to update their profile, but the user id is undefined.`,
-      )
-      throw new Error(i18n._(t`Authentication error, please sign in again.`))
-    }
+      // Make sure userId is not undefined
+      if (typeof userId === 'undefined') {
+        console.warn(
+          `User attempted to update their profile, but the user id is undefined.`,
+        )
+        throw new Error(i18n._(t`Authentication error, please sign in again.`))
+      }
 
-    // Get user info from DB
-    const user = await userLoaderByKey.load(userId)
+      // Get user info from DB
+      const user = await userLoaderByKey.load(userId)
 
-    if (typeof user === 'undefined') {
-      console.warn(
-        `User: ${userId} attempted to update their profile, but no account is associated with that id.`,
-      )
-      throw new Error(i18n._(t`Unable to update profile. Please try again.`))
-    }
+      if (typeof user === 'undefined') {
+        console.warn(
+          `User: ${userId} attempted to update their profile, but no account is associated with that id.`,
+        )
+        throw new Error(i18n._(t`Unable to update profile. Please try again.`))
+      }
 
-    // Create object containing updated data
-    const updatedUser = {
-      displayName: displayName || user.displayName,
-      userName: userName || user.userName,
-      preferredLang: preferredLang || user.preferredLang,
-    }
+      // Create object containing updated data
+      const updatedUser = {
+        displayName: displayName || user.displayName,
+        userName: userName || user.userName,
+        preferredLang: preferredLang || user.preferredLang,
+      }
 
-    try {
-      await query`
+      try {
+        await query`
         UPSERT { _key: ${user._key} }
           INSERT ${updatedUser}
           UPDATE ${updatedUser} 
           IN users
       `
-    } catch (err) {
-      console.error(
-        `Database error ocurred when user: ${user._key} attempted to update their profile: ${err}`,
-      )
-      throw new Error(i18n._(t`Unable to update profile. Please try again.`))
-    }
+      } catch (err) {
+        console.error(
+          `Database error ocurred when user: ${user._key} attempted to update their profile: ${err}`,
+        )
+        throw new Error(i18n._(t`Unable to update profile. Please try again.`))
+      }
 
-    console.info(`User: ${user._key} successfully updated their profile.`)
-    return {
-      status: i18n._(t`Profile successfully updated.`),
-    }
-  },
-})
+      console.info(`User: ${user._key} successfully updated their profile.`)
+      return {
+        status: i18n._(t`Profile successfully updated.`),
+      }
+    },
+  })
 
 module.exports = {
   updateUserProfile,
