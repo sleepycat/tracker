9bb014d8438bf49ec70d3f65d97907db0477dae0
nsdeschenes <nsdeschenes@outlook.com>
2020-11-03 08:06:52 -0400

    slug scalar translated

--- api-js/src/scalars/slug.js
+++ api-js/src/scalars/slug.js
@@ -1,31 +1,39 @@
+const { t } = require('@lingui/macro')
 const { Kind, GraphQLError, GraphQLScalarType } = require('graphql')
 
-const validate = (value) => {
+const validate = (i18n) => (value) => {
   const SLUG_REGEX = /^[a-z0-9]+(?:-[a-z0-9]+)*$/
 
   if (typeof value !== 'string') {
-    throw new TypeError(`Value is not string: ${typeof value}`)
+    throw new TypeError(i18n._(t`Value is not string: ${typeof value}`))
   }
 
   if (!SLUG_REGEX.test(value)) {
-    throw new TypeError(`Value is not a valid slug: ${value}`)
+    throw new TypeError(i18n._(t`Value is not a valid slug: ${value}`))
   }
   return value
 }
 
-module.exports.Slug = new GraphQLScalarType({
-  name: 'Slug',
-  description:
-    'A field whos values contain numbers, letters, dashes, and underscores.',
-  serialize: validate,
-  parseValue: validate,
+const parseLiteral = (i18n) => (ast) => {
+  if (ast.kind !== Kind.STRING) {
+    throw new GraphQLError(
+      i18n._(t`Can only validate strings as slug but got a: ${ast.kind}`),
+    )
+  }
+  return validate(i18n)(ast.value)
+}
 
-  parseLiteral(ast) {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError(
-        `Can only validate strings as slug but got a: ${ast.kind}`,
-      )
-    }
-    return validate(ast.value)
-  },
-})
+const Slug = (i18n) =>
+  new GraphQLScalarType({
+    name: 'Slug',
+    description: i18n._(
+      t`A field who's values contain numbers, letters, dashes, and underscores.`,
+    ),
+    serialize: validate(i18n),
+    parseValue: validate(i18n),
+    parseLiteral: parseLiteral(i18n),
+  })
+
+module.exports = {
+  Slug,
+}
