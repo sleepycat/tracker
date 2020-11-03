de05dbcf4bb92fc7807ad014ae2a6ecb2529763e
nsdeschenes <nsdeschenes@outlook.com>
2020-11-03 08:06:42 -0400

    selector scalar translated

--- api-js/src/__tests__/scalar-selector.test.js
+++ api-js/src/__tests__/scalar-selector.test.js
@@ -1,115 +1,275 @@
 const { Kind } = require('graphql')
 const { stringify } = require('jest-matcher-utils')
+const { setupI18n } = require('@lingui/core')
+
+const englishMessages = require('../locale/en/messages')
+const frenchMessages = require('../locale/fr/messages')
 const { Selectors } = require('../scalars')
 
-describe('given a selectors scalar', () => {
-  describe('serializing inputs', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid selector', () => {
-        it('returns test selector', () => {
-          const testSelector = 'selector1._domainkey'
-          expect(Selectors.serialize(testSelector)).toEqual(testSelector)
+let i18n
+describe('language is set to english', () => {
+  beforeAll(() => {
+    i18n = setupI18n({
+      language: 'en',
+      locales: ['en', 'fr'],
+      missing: 'Traduction manquante',
+      catalogs: {
+        en: englishMessages,
+        fr: frenchMessages,
+      },
+    })
+  })
+  describe('given a selectors scalar', () => {
+    describe('serializing inputs', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
+            expect(Selectors(i18n).serialize(testSelector)).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              expect(() => Selectors(i18n).serialize(testSelector)).toThrow(
+                new TypeError(`Value is not a valid selector: ${testSelector}`),
+              )
+            })
+          })
         })
       })
-      describe('given an invalid selector', () => {
-        describe('selector contains string', () => {
-          it('throws an error', () => {
-            const testSelector = 'This is an invalid selector'
-            expect(() => Selectors.serialize(testSelector)).toThrow(
-              new TypeError(`Value is not a valid selector: ${testSelector}`),
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Selectors(i18n).serialize(invalidInput)).toThrow(
+              new TypeError(`Value is not a string: ${typeof invalidInput}`),
             )
           })
         })
       })
     })
-    describe('given invalid inputs', () => {
-      ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
-        it(`throws an error when serializing ${stringify(
-          invalidInput,
-        )}`, () => {
-          expect(() => Selectors.serialize(invalidInput)).toThrow(
-            new TypeError(`Value is not a string: ${typeof invalidInput}`),
-          )
+    describe('value parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
+            expect(Selectors(i18n).parseValue(testSelector)).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              expect(() => Selectors(i18n).parseValue(testSelector)).toThrow(
+                new TypeError(`Value is not a valid selector: ${testSelector}`),
+              )
+            })
+          })
         })
       })
-    })
-  })
-  describe('value parsing', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid selector', () => {
-        it('returns test selector', () => {
-          const testSelector = 'selector1._domainkey'
-          expect(Selectors.parseValue(testSelector)).toEqual(testSelector)
-        })
-      })
-      describe('given an invalid selector', () => {
-        describe('selector contains string', () => {
-          it('throws an error', () => {
-            const testSelector = 'This is an invalid selector'
-            expect(() => Selectors.parseValue(testSelector)).toThrow(
-              new TypeError(`Value is not a valid selector: ${testSelector}`),
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Selectors(i18n).parseValue(invalidInput)).toThrow(
+              new TypeError(`Value is not a string: ${typeof invalidInput}`),
             )
           })
         })
       })
     })
-    describe('given invalid inputs', () => {
-      ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
-        it(`throws an error when serializing ${stringify(
-          invalidInput,
-        )}`, () => {
-          expect(() => Selectors.parseValue(invalidInput)).toThrow(
-            new TypeError(`Value is not a string: ${typeof invalidInput}`),
-          )
-        })
-      })
-    })
-  })
-  describe('literal parsing', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid selector', () => {
-        it('returns test selector', () => {
-          const testSelector = 'selector1._domainkey'
-          const testLiteral = {
-            kind: Kind.STRING,
-            value: testSelector,
-          }
-          expect(Selectors.parseLiteral(testLiteral, {})).toEqual(testSelector)
-        })
-      })
-      describe('given an invalid selector', () => {
-        describe('selector contains string', () => {
-          it('throws an error', () => {
-            const testSelector = 'This is an invalid selector'
+    describe('literal parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
             const testLiteral = {
               kind: Kind.STRING,
               value: testSelector,
             }
-            expect(() => Selectors.parseLiteral(testLiteral, {})).toThrow(
-              new TypeError(`Value is not a valid selector: ${testSelector}`),
+            expect(Selectors(i18n).parseLiteral(testLiteral, {})).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              const testLiteral = {
+                kind: Kind.STRING,
+                value: testSelector,
+              }
+              expect(() =>
+                Selectors(i18n).parseLiteral(testLiteral, {}),
+              ).toThrow(
+                new TypeError(`Value is not a valid selector: ${testSelector}`),
+              )
+            })
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[
+          {
+            kind: Kind.FLOAT,
+            value: '5',
+          },
+          {
+            kind: Kind.DOCUMENT,
+          },
+        ].forEach((literal) => {
+          it(`throws an error when parsing invalid literal ${stringify(
+            literal,
+          )}`, () => {
+            expect(() => Selectors(i18n).parseLiteral(literal, {})).toThrow(
+              new TypeError(
+                `Can only validate strings as selectors but got a: ${literal.kind}`,
+              ),
             )
           })
         })
       })
     })
-    describe('given invalid inputs', () => {
-      ;[
-        {
-          kind: Kind.FLOAT,
-          value: '5',
-        },
-        {
-          kind: Kind.DOCUMENT,
-        },
-      ].forEach((literal) => {
-        it(`throws an error when parsing invalid literal ${stringify(
-          literal,
-        )}`, () => {
-          expect(() => Selectors.parseLiteral(literal, {})).toThrow(
-            new TypeError(
-              `Can only validate strings as selectors but got a: ${literal.kind}`,
-            ),
-          )
+  })
+})
+describe('language is set to french', () => {
+  beforeAll(() => {
+    i18n = setupI18n({
+      language: 'fr',
+      locales: ['en', 'fr'],
+      missing: 'Traduction manquante',
+      catalogs: {
+        en: englishMessages,
+        fr: frenchMessages,
+      },
+    })
+  })
+  describe('given a selectors scalar', () => {
+    describe('serializing inputs', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
+            expect(Selectors(i18n).serialize(testSelector)).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              expect(() => Selectors(i18n).serialize(testSelector)).toThrow(
+                new TypeError(`todo`),
+              )
+            })
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Selectors(i18n).serialize(invalidInput)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
+      })
+    })
+    describe('value parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
+            expect(Selectors(i18n).parseValue(testSelector)).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              expect(() => Selectors(i18n).parseValue(testSelector)).toThrow(
+                new TypeError(`todo`),
+              )
+            })
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Selectors(i18n).parseValue(invalidInput)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
+      })
+    })
+    describe('literal parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid selector', () => {
+          it('returns test selector', () => {
+            const testSelector = 'selector1._domainkey'
+            const testLiteral = {
+              kind: Kind.STRING,
+              value: testSelector,
+            }
+            expect(Selectors(i18n).parseLiteral(testLiteral, {})).toEqual(
+              testSelector,
+            )
+          })
+        })
+        describe('given an invalid selector', () => {
+          describe('selector contains string', () => {
+            it('throws an error', () => {
+              const testSelector = 'This is an invalid selector'
+              const testLiteral = {
+                kind: Kind.STRING,
+                value: testSelector,
+              }
+              expect(() =>
+                Selectors(i18n).parseLiteral(testLiteral, {}),
+              ).toThrow(
+                new TypeError(`todo`),
+              )
+            })
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[
+          {
+            kind: Kind.FLOAT,
+            value: '5',
+          },
+          {
+            kind: Kind.DOCUMENT,
+          },
+        ].forEach((literal) => {
+          it(`throws an error when parsing invalid literal ${stringify(
+            literal,
+          )}`, () => {
+            expect(() => Selectors(i18n).parseLiteral(literal, {})).toThrow(
+              new TypeError(
+                `todo`,
+              ),
+            )
+          })
         })
       })
     })
--- api-js/src/scalars/selector.js
+++ api-js/src/scalars/selector.js
@@ -1,30 +1,38 @@
+const { t } = require('@lingui/macro')
 const { Kind, GraphQLError, GraphQLScalarType } = require('graphql')
 
-const validate = (value) => {
+const validate = (i18n) => (value) => {
   const SLUG_REGEX = /\w+\._domainkey/
   if (typeof value !== typeof 'string') {
-    throw new TypeError(`Value is not a string: ${typeof value}`)
+    throw new TypeError(i18n._(t`Value is not a string: ${typeof value}`))
   }
   if (!SLUG_REGEX.test(value)) {
-    throw new TypeError(`Value is not a valid selector: ${value}`)
+    throw new TypeError(i18n._(t`Value is not a valid selector: ${value}`))
   }
 
   return value
 }
 
-module.exports.Selectors = new GraphQLScalarType({
-  name: 'Selector',
-  description:
-    'A field that conforms to a string, with strings ending in ._domainkey.',
-  serialize: validate,
-  parseValue: validate,
+const parseLiteral = (i18n) => (ast) => {
+  if (ast.kind !== Kind.STRING) {
+    throw new GraphQLError(
+      i18n._(t`Can only validate strings as selectors but got a: ${ast.kind}`),
+    )
+  }
+  return validate(i18n)(ast.value)
+}
 
-  parseLiteral(ast) {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError(
-        `Can only validate strings as selectors but got a: ${ast.kind}`,
-      )
-    }
-    return validate(ast.value)
-  },
-})
+const Selectors = (i18n) =>
+  new GraphQLScalarType({
+    name: 'Selector',
+    description: i18n._(
+      t`A field that conforms to a string, with strings ending in ._domainkey.`,
+    ),
+    serialize: validate(i18n),
+    parseValue: validate(i18n),
+    parseLiteral: parseLiteral(i18n),
+  })
+
+module.exports = {
+  Selectors,
+}
