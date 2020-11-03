10bcc1df8321f445e261044c07bc45a2e6966f85
nsdeschenes <nsdeschenes@outlook.com>
2020-11-03 07:57:24 -0400

    domain scalar translated

--- api-js/src/__tests__/scalar-domain.test.js
+++ api-js/src/__tests__/scalar-domain.test.js
@@ -1,101 +1,249 @@
 const { Kind } = require('graphql')
 const { stringify } = require('jest-matcher-utils')
+const { setupI18n } = require('@lingui/core')
+
+const englishMessages = require('../locale/en/messages')
+const frenchMessages = require('../locale/fr/messages')
 const { Domain } = require('../scalars')
 
-describe('given a domain scalar', () => {
-  describe('serializing inputs', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid domain', () => {
-        it('returns test domain', () => {
-          const testDomain = 'test.domain.ca'
-          expect(Domain.serialize(testDomain)).toEqual(testDomain)
+let i18n
+describe('language is set to english', () => {
+  beforeAll(() => {
+    i18n = setupI18n({
+      language: 'en',
+      locales: ['en', 'fr'],
+      missing: 'Traduction manquante',
+      catalogs: {
+        en: englishMessages,
+        fr: frenchMessages,
+      },
+    })
+  })
+  describe('given a domain scalar', () => {
+    describe('serializing inputs', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns test domain', () => {
+            const testDomain = 'test.domain.ca'
+            expect(Domain(i18n).serialize(testDomain)).toEqual(testDomain)
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws type error', () => {
+            const testDomain = 'not an domain'
+            expect(() => Domain(i18n).serialize(testDomain)).toThrow(
+              new TypeError(`Value is not a valid domain: ${testDomain}`),
+            )
+          })
         })
       })
-      describe('given an invalid domain', () => {
-        it('throws type error', () => {
-          const testDomain = 'not an domain'
-          expect(() => Domain.serialize(testDomain)).toThrow(
-            new TypeError(`Value is not a valid domain: ${testDomain}`),
-          )
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Domain(i18n).serialize(invalidInput)).toThrow(
+              new TypeError(`Value is not a string: ${typeof invalidInput}`),
+            )
+          })
         })
       })
     })
-    describe('given invalid inputs', () => {
-      ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
-        it(`throws an error when serializing ${stringify(
-          invalidInput,
-        )}`, () => {
-          expect(() => Domain.serialize(invalidInput)).toThrow(
-            new TypeError(`Value is not a string: ${typeof invalidInput}`),
-          )
+    describe('value parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns the parsed domain', () => {
+            const testDomain = 'test.domain.ca'
+            expect(Domain(i18n).parseValue(testDomain)).toEqual(testDomain)
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws an error', () => {
+            const testDomain = 'not an domain'
+            expect(() => Domain(i18n).parseValue(testDomain)).toThrow(
+              new TypeError(`Value is not a valid domain: ${testDomain}`),
+            )
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Domain(i18n).parseValue(invalidInput)).toThrow(
+              new TypeError(`Value is not a string: ${typeof invalidInput}`),
+            )
+          })
+        })
+      })
+    })
+    describe('literal parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns the parsed domain', () => {
+            const testDomain = 'test.domain.ca'
+            const testLiteral = {
+              kind: Kind.STRING,
+              value: testDomain,
+            }
+            expect(Domain(i18n).parseLiteral(testLiteral, {})).toEqual(
+              testDomain,
+            )
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws an error', () => {
+            const testDomain = 'not an domain'
+            const testLiteral = {
+              kind: Kind.STRING,
+              value: testDomain,
+            }
+            expect(() => Domain(i18n).parseLiteral(testLiteral, {})).toThrow(
+              new TypeError(`Value is not a valid domain: ${testDomain}`),
+            )
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[
+          {
+            kind: Kind.FLOAT,
+            value: '5',
+          },
+          {
+            kind: Kind.DOCUMENT,
+          },
+        ].forEach((literal) => {
+          it(`throws an error when parsing invalid literal ${stringify(
+            literal,
+          )}`, () => {
+            expect(() => Domain(i18n).parseLiteral(literal, {})).toThrow(
+              new TypeError(
+                `Can only validate strings as domains but got a: ${literal.kind}`,
+              ),
+            )
+          })
         })
       })
     })
   })
-  describe('value parsing', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid domain', () => {
-        const testDomain = 'test.domain.ca'
-        expect(Domain.parseValue(testDomain)).toEqual(testDomain)
-      })
-      describe('given an invalid domain', () => {
-        const testDomain = 'not an domain'
-        expect(() => Domain.parseValue(testDomain)).toThrow(
-          new TypeError(`Value is not a valid domain: ${testDomain}`),
-        )
-      })
+})
+describe('language is set to french', () => {
+  beforeAll(() => {
+    i18n = setupI18n({
+      language: 'fr',
+      locales: ['en', 'fr'],
+      missing: 'Traduction manquante',
+      catalogs: {
+        en: englishMessages,
+        fr: frenchMessages,
+      },
     })
-    describe('given invalid inputs', () => {
-      ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
-        it(`throws an error when serializing ${stringify(
-          invalidInput,
-        )}`, () => {
-          expect(() => Domain.parseValue(invalidInput)).toThrow(
-            new TypeError(`Value is not a string: ${typeof invalidInput}`),
-          )
+  })
+  describe('given a domain scalar', () => {
+    describe('serializing inputs', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns test domain', () => {
+            const testDomain = 'test.domain.ca'
+            expect(Domain(i18n).serialize(testDomain)).toEqual(testDomain)
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws type error', () => {
+            const testDomain = 'not an domain'
+            expect(() => Domain(i18n).serialize(testDomain)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Domain(i18n).serialize(invalidInput)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
         })
       })
     })
-  })
-  describe('literal parsing', () => {
-    describe('given valid inputs', () => {
-      describe('given a valid domain', () => {
-        const testDomain = 'test.domain.ca'
-        const testLiteral = {
-          kind: Kind.STRING,
-          value: testDomain,
-        }
-        expect(Domain.parseLiteral(testLiteral, {})).toEqual(testDomain)
+    describe('value parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns the parsed domain', () => {
+            const testDomain = 'test.domain.ca'
+            expect(Domain(i18n).parseValue(testDomain)).toEqual(testDomain)
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws an error', () => {
+            const testDomain = 'not an domain'
+            expect(() => Domain(i18n).parseValue(testDomain)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
       })
-      describe('given an invalid domain', () => {
-        const testDomain = 'not an domain'
-        const testLiteral = {
-          kind: Kind.STRING,
-          value: testDomain,
-        }
-        expect(() => Domain.parseLiteral(testLiteral, {})).toThrow(
-          new TypeError(`Value is not a valid domain: ${testDomain}`),
-        )
+      describe('given invalid inputs', () => {
+        ;[123, {}, [], null, undefined, true].forEach((invalidInput) => {
+          it(`throws an error when serializing ${stringify(
+            invalidInput,
+          )}`, () => {
+            expect(() => Domain(i18n).parseValue(invalidInput)).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
       })
     })
-    describe('given invalid inputs', () => {
-      ;[
-        {
-          kind: Kind.FLOAT,
-          value: '5',
-        },
-        {
-          kind: Kind.DOCUMENT,
-        },
-      ].forEach((literal) => {
-        it(`throws an error when parsing invalid literal ${stringify(
-          literal,
-        )}`, () => {
-          expect(() => Domain.parseLiteral(literal, {})).toThrow(
-            new TypeError(
-              `Can only validate strings as domains but got a: ${literal.kind}`,
-            ),
-          )
+    describe('literal parsing', () => {
+      describe('given valid inputs', () => {
+        describe('given a valid domain', () => {
+          it('returns the parsed domain', () => {
+            const testDomain = 'test.domain.ca'
+            const testLiteral = {
+              kind: Kind.STRING,
+              value: testDomain,
+            }
+            expect(Domain(i18n).parseLiteral(testLiteral, {})).toEqual(
+              testDomain,
+            )
+          })
+        })
+        describe('given an invalid domain', () => {
+          it('throws an error', () => {
+            const testDomain = 'not an domain'
+            const testLiteral = {
+              kind: Kind.STRING,
+              value: testDomain,
+            }
+            expect(() => Domain(i18n).parseLiteral(testLiteral, {})).toThrow(
+              new TypeError(`todo`),
+            )
+          })
+        })
+      })
+      describe('given invalid inputs', () => {
+        ;[
+          {
+            kind: Kind.FLOAT,
+            value: '5',
+          },
+          {
+            kind: Kind.DOCUMENT,
+          },
+        ].forEach((literal) => {
+          it(`throws an error when parsing invalid literal ${stringify(
+            literal,
+          )}`, () => {
+            expect(() => Domain(i18n).parseLiteral(literal, {})).toThrow(
+              new TypeError(`todo`),
+            )
+          })
         })
       })
     })
--- api-js/src/scalars/domain.js
+++ api-js/src/scalars/domain.js
@@ -1,29 +1,36 @@
+const { t } = require('@lingui/macro')
 const { Kind, GraphQLError, GraphQLScalarType } = require('graphql')
 const psl = require('psl')
 
-const validate = (value) => {
+const validate = (i18n) => (value) => {
   if (typeof value !== typeof 'string') {
-    throw new TypeError(`Value is not a string: ${typeof value}`)
+    throw new TypeError(i18n._(t`Value is not a string: ${typeof value}`))
   }
   if (!psl.isValid(value)) {
-    throw new TypeError(`Value is not a valid domain: ${value}`)
+    throw new TypeError(i18n._(t`Value is not a valid domain: ${value}`))
   }
 
   return value
 }
 
-module.exports.Domain = new GraphQLScalarType({
-  name: 'DomainScalar',
-  description: 'String that conforms to a domain structure.',
-  serialize: validate,
-  parseValue: validate,
+const parseLiteral = (i18n) => (ast) => {
+  if (ast.kind !== Kind.STRING) {
+    throw new GraphQLError(
+      i18n._(t`Can only validate strings as domains but got a: ${ast.kind}`),
+    )
+  }
+  return validate(i18n)(ast.value)
+}
 
-  parseLiteral(ast) {
-    if (ast.kind !== Kind.STRING) {
-      throw new GraphQLError(
-        `Can only validate strings as domains but got a: ${ast.kind}`,
-      )
-    }
-    return validate(ast.value)
-  },
+const Domain = (i18n) => new GraphQLScalarType({
+  name: 'DomainScalar',
+  description: i18n._(t`String that conforms to a domain structure.`),
+  serialize: validate(i18n),
+  parseValue: validate(i18n),
+  parseLiteral: parseLiteral(i18n),
 })
+
+
+module.exports = {
+  Domain,
+}
