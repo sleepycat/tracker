119402bd08ed6ec9d38132802663e8259fd0fedc
nsdeschenes <nsdeschenes@outlook.com>
2020-11-03 07:24:59 -0400

    scalar imports

--- api-js/src/scalars/index.js
+++ api-js/src/scalars/index.js
@@ -3,6 +3,11 @@ const { Domain } = require('./domain')
 const { Selectors } = require('./selector.js')
 const { Slug } = require('./slug.js')
 const { Year } = require('./year.js')
+const { TranslatedBoolean } = require('./translated-boolean')
+const { TranslatedFloat } = require('./translated-float')
+const { TranslatedID } = require('./translated-id')
+const { TranslatedInt } = require('./translated-int')
+const { TranslatedString } = require('./translated-string')
 
 module.exports = {
   Acronym,
@@ -10,4 +15,9 @@ module.exports = {
   Selectors,
   Slug,
   Year,
+  TranslatedBoolean,
+  TranslatedFloat,
+  TranslatedID,
+  TranslatedInt,
+  TranslatedString,
 }
