a0062286d109410794ba8756d4f5daab19989c16
nsdeschenes <nsdeschenes@outlook.com>
2020-11-04 08:53:16 -0400

    translation files

--- api-js/src/types/auth-result.js
+++ api-js/src/types/auth-result.js
@@ -1,20 +1,22 @@
-const { GraphQLObjectType, GraphQLString } = require('graphql')
+const { GraphQLObjectType } = require('graphql')
+const { t } = require('@lingui/macro')
 const { userType } = require('./base')
+const { TranslatedString } = require('../scalars')
 
-const authResultType = new GraphQLObjectType({
+const authResultType = (i18n) => new GraphQLObjectType({
   name: 'AuthResult',
-  description: `An object used to return information when users sign up or authenticate.`,
+  description: i18n._(t`An object used to return information when users sign up or authenticate.`),
   fields: () => ({
     authToken: {
-      type: GraphQLString,
-      description: `JWT used for accessing controlled content.`,
+      type: TranslatedString(i18n),
+      description: i18n._(t`JWT used for accessing controlled content.`),
       resolve: async ({ token }) => {
         return token
       },
     },
     user: {
-      type: userType,
-      description: `User that has just been created or signed in.`,
+      type: userType(i18n),
+      description: i18n._(t`User that has just been created or signed in.`),
       resolve: async ({ user }) => {
         return user
       },
