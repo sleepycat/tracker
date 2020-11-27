175cc3cc82b120b1fdded4fa89adb39d837ec868
nsdeschenes <nsdeschenes@outlook.com>
2020-11-27 13:16:08 -0400

    move create context function to a factory function

--- api-js/src/__tests__/create-context.test.js
+++ api-js/src/__tests__/create-context.test.js
@@ -4,8 +4,7 @@ const { tokenize } = require('../auth')
 describe('given the create context function', () => {
   describe('request authorization token is not set', () => {
     it('returns object with userKey as undefined', async () => {
-      const context = createContext({
-        context: {},
+      const context = await createContext({})({
         req: { headers: { authorization: '' } },
         res: {},
       })
@@ -16,13 +15,33 @@ describe('given the create context function', () => {
   describe('request authorization token is set', () => {
     it('returns object with userKey as value', async () => {
       const token = tokenize({ parameters: { userKey: '1234' } })
-      const context = createContext({
-        context: {},
+      const context = await createContext({})({
         req: { headers: { authorization: token } },
         res: {},
       })
 
       expect(context.userKey).toEqual('1234')
+    })
+  })
+  describe('connection value is set', () => {
+    it('returns an object with set token', async () => {
+      const token = tokenize({ parameters: { userKey: '1234' } })
+      const context = await createContext({})({
+        req: {},
+        res: {},
+        connection: { context: { authorization: token } },
+      })
+
+      expect(context.userKey).toEqual({ userKey: '1234' })
+    })
+    it('returns an object with a set language', async () => {
+      const context = await createContext({})({
+        req: {},
+        res: {},
+        connection: { context: { language: 'en' } },
+      })
+
+      expect(context.request.language).toEqual('en')
     })
   })
 })
